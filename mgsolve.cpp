468aa613807db3118e11cc1fde131d051a937403
Juliankarl33 <bu43jazu@stud.informatik.uni-erlangen.de>
2016-04-26 15:19:39 +0200

    added multigrid method

--- mgsolve.cpp
+++ mgsolve.cpp
@@ -26,19 +26,30 @@ double residuum(int nx, int ny, std::vector<double> &grid, std::vector<double> &
     return residuum;
 }
 
-void coarsening( l, std::vector<std::vector<double>> grid, std::vector<double> nx, std::vector<double> ny ){
+void residual( int l, int nx, int ny, std::vector<double> &grid, std::vector<double> &f_x_y, std::vector<double> &res, double h){
+    for(int i=1; i<ny-1; i++){
+        for(int k=1; k<nx-1; k++){
+                res[i*nx+k] = ((-1.0/h)*(grid[i*nx+(k-1)]+grid[i*nx+(k+1)]) - (1.0/h)*(grid[(i-1)*nx+k]+grid[(i+1)*nx+k]) + ((2.0/h+2.0/h)*grid[i*nx+k]) - f_x_y[i*nx+k]);
+        }
+    }
+}
+
+// restrict a grid (from) with the full weighting stencile to another grid (to). From has size nx[l] and to has size nx[l-1].
+void coarsening( int l, std::vector<double>& from, std::vector<double>& to, std::vector<double>& nx, std::vector<double>& ny ){
     
   for( int i=0; i<nx[l-1]; i++ ){
     for( int j=0; j<ny[l-1]; j++ ){
-      
+      to[i*nx[l-1]+j] = (   from[(2*i+1)*nx[l]+2*j-1] + 2*from[(2*i+1)*nx[l]+2*j] +   from[(2*i+1)*nx[l]+2*j+1]
+			+ 2*from[2*i*nx[l]+2*j-1]     + 4*from[2*i*nx[l]+2*j]     + 2*from[2*i*nx[l]+2*j+1]
+			+   from[(2*i-1)*nx[l]+2*j-1] + 2*from[(2*i-1)*nx[l]+2*j] +   from[(2*i-1)*nx[l]+2*j+1] ) /16.0;
     }
   }
 }
 
 
-void Red_Black_Gauss_v(int nx, int ny, std::vector<double> &grid, std::vector<double> &f_x_y, double h){
+void Red_Black_Gauss(int nx, int ny, std::vector<double> &grid, std::vector<double> &f_x_y, double h, int numIterations){
     
-     for(int iterations=0; iterations<nx*ny; iterations++){
+     for(int iterations=0; iterations<numIterations; iterations++){
 		for(int m=1; m<ny-1; m++){
 			int q=1;
 			if(m%2 == 0){
@@ -58,6 +69,41 @@ void Red_Black_Gauss_v(int nx, int ny, std::vector<double> &grid, std::vector<do
 			}
 		}
 	}
+}
+
+void multigrid( int l, std::vector<std::vector<double>>& grid, std::vector<std::vector<double>>& f, std::vector<double>& nx,
+		std::vector<double>& ny, std::vector<double>& h, std::vector<std::vector<double>>& res, int v1=2, int v2=1, int gamma=1 ){
+  
+  // l = 0 entspricht Level 1 
+  l--;
+  
+  //Presmoothing
+  Red_Black_Gauss( nx[l], ny[l], grid[l], f[l], h[l], v1 );
+  
+  // Residuum
+  residual( l, nx[l], ny[l], grid[l], f[l], res[l], h[l] );
+  
+  // restrict residual
+  coarsening( l, res[l], f[l-1], nx, ny );
+  
+  if( l == 1 ){
+    Red_Black_Gauss( nx[l-1], ny[l-1], grid[l-1], f[l-1], h[l-1], 10 );
+  }else{
+    for( int i=0; i<gamma; i++ ){
+      multigrid( l, grid, f, nx, ny, h, res, v1, v2, gamma );
+    }
+    // interpolation
+    std::vector<double> c( nx[l]*ny[l], 0.0 );
+    interpolate( c2h to c );
+    for( int i=0; i<nx[l-1]; i++ ){
+      for( int j=0; j<ny[l-1]; j++ ){
+	grid[l][i*nx[l]+j] += c[i*nx[l]+j];
+      }
+    }
+  }
+  
+  //Postsmothing
+  Red_Black_Gauss( nx[l], ny[l], grid[l], f[l], h[l], v2 );
 }
 
 int main(int argc, char **argv){
@@ -96,6 +142,12 @@ int main(int argc, char **argv){
 	f_x_y[i] = std::vector<double>(nx[i]*ny[i],0.0);
     }
     
+    // Speicher fuer das residual allokieren
+    std::vector<std::vector<double>> res(l);
+    for( int i=l-1; i>=0; i--){
+      res[i] = std::vector<double>(nx[i]*ny[i], 0.0);
+    }
+  
     // Speicher fuer das Gitter allokieren:
     std::vector<std::vector<double>> grid(l);
     
@@ -132,13 +184,13 @@ int main(int argc, char **argv){
     struct timeval t1;
     gettimeofday(&t1, NULL);
 
-    Red_Black_Gauss_v(nx[l-1], ny[l-1], grid[l-1], f_x_y[l-1], h[l-1]);
+    Red_Black_Gauss(nx[l-1], ny[l-1], grid[l-1], f_x_y[l-1], h[l-1], 1000);
 
     struct timeval t2;
     gettimeofday(&t2, NULL);
     fprintf(stdout, "Timer Red_Black_Gauss: %lf\n", timevalToDouble(&t2)-timevalToDouble(&t1));
 
-    double res = residuum(nx[l-1], ny[l-1], grid[l-1], f_x_y[l-1], h[l-1]);
+    double resid = residuum(nx[l-1], ny[l-1], grid[l-1], f_x_y[l-1], h[l-1]);
 
     // Oeffnen der Ausgabedatei
 	FILE *out = fopen("./solution.txt", "w");
@@ -159,5 +211,5 @@ int main(int argc, char **argv){
 	fprintf(out, "\n");
     }
     fclose(out);
-    fprintf(stdout, "Residuum: %lf\n", res);
+    fprintf(stdout, "Residuum: %lf\n", resid);
 }
